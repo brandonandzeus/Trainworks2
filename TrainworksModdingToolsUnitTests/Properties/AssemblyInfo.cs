4181f88c1eca51792a02d9a36c2ce32582a5c9b1
Brandon <tricksterguy87@gmail.com>
2023-09-17 00:50:44 -0700

    Versioning touchups.

--- TrainworksModdingToolsUnitTests/Properties/AssemblyInfo.cs
+++ TrainworksModdingToolsUnitTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("899368bb-14f8-410a-8265-0939e82948ac")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

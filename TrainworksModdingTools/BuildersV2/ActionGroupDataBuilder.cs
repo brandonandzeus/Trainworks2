fb03a001bec8dcfa904e082c7f0866e30de7d4ae
Brandon <tricksterguy87@gmail.com>
2023-10-26 00:17:32 -0700

    Builders for Boss Action stuff.

---
+++ TrainworksModdingTools/BuildersV2/ActionGroupDataBuilder.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.BuildersV2
+{
+    /// <summary>
+    /// This class controls the actions for flying bosses.
+    /// </summary>
+    public class ActionGroupDataBuilder
+    {
+        /// <summary>
+        /// ???
+        /// </summary>
+        public int NumRepeats { get; set; }
+        /// <summary>
+        /// Boss Actions.
+        /// </summary>
+        public List<BossActionData> Actions { get; set; }
+        /// <summary>
+        /// Convienence Builder for Actions. Will be appeneded to Actions.
+        /// </summary>
+        public List<BossActionDataBuilder> ActionBuilders { get; set; }
+
+        public ActionGroupDataBuilder()
+        {
+            NumRepeats = 1;
+            Actions = new List<BossActionData>();
+            ActionBuilders = new List<BossActionDataBuilder>();
+        }
+
+        public ActionGroupData Build()
+        {
+            var actionGroupData = new ActionGroupData();
+
+            var actions = new List<BossActionData>();
+            actions.AddRange(Actions);
+            foreach (var action in ActionBuilders)
+                actions.Add(action.Build());
+
+            AccessTools.Field(typeof(ActionGroupData), "_numRepeats").SetValue(actionGroupData, NumRepeats);
+            AccessTools.Field(typeof(ActionGroupData), "_actions").SetValue(actionGroupData, actions);
+
+            return actionGroupData;
+        }
+    }
+}

6601892697d08c5856879c3ef32d08f5b7c85c41
brandonandzeus <101950394+brandonandzeus@users.noreply.github.com>
2023-11-06 14:08:53 -0800

    Update TrainworksModdingTools.cs
    Version 2.1

--- TrainworksModdingTools/TrainworksModdingTools.cs
+++ TrainworksModdingTools/TrainworksModdingTools.cs
@@ -24,7 +24,7 @@ namespace Trainworks
     {
         public const string GUID = "tools.modding.trainworks";
         public const string NAME = "Trainworks Modding Tools";
-        public const string VERSION = "2.0.0";
+        public const string VERSION = "2.1.0";
 
         /// <summary>
         /// The framework's logging source.

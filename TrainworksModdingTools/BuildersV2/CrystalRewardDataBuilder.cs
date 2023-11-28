8fb01e0b1599d980b8b85ce95def726becce42f5
Brandon <tricksterguy87@gmail.com>
2023-11-27 21:05:59 -0800

    Add stubbed out builders for RewardData objects.

---
+++ TrainworksModdingTools/BuildersV2/CrystalRewardDataBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainworksModdingTools.BuildersV2
+{
+    public class CrystalRewardDataBuilder
+    {
+        public RewardData Build(bool register = true)
+        {
+            return null;
+        }
+
+        public RewardData BuildAndRegister()
+        {
+            return null;
+        }
+    }
+}
---
+++ TrainworksModdingTools/BuildersV2/PurgeRewardDataBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.BuildersV2
+{
+    public class PurgeRewardDataBuilder
+    {
+        public RewardData Build(bool register = true)
+        {
+            return null;
+        }
+
+        public RewardData BuildAndRegister()
+        {
+            return null;
+        }
+    }
+}

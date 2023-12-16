6c2aaf8fe6af731e0c8417211d44b3390cb1a3f4
Brandon <tricksterguy87@gmail.com>
2023-12-16 15:29:07 -0800

    New functions for CustomCharacterManager
    AddCustomRoomModifierIcon Adds a icon to be shared across instances of RoomModifierBase subclasses. Included a patch to get it to work with minimal disruption.
    AddCustomTriggerIcon Adds a icon for a Custom Character Trigger.

---
+++ TrainworksModdingTools/Patches/RoomModifierIconPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.Managers;
+using UnityEngine;
+
+namespace Trainworks.Patches
+{
+    [HarmonyPatch(typeof(RoomStateModifierBase), nameof(RoomStateModifierBase.Initialize))]
+    public class RoomModifierIconPatch
+    {
+        public static void Postfix(RoomModifierData roomModifierData, ref Sprite ___icon)
+        {
+            // Nothing to do.
+            if (___icon != null)
+                return;
+
+            foreach (var class_name_sprite in CustomCharacterManager.CustomRoomModifierIcons)
+            {
+                if (class_name_sprite.Key == roomModifierData.GetRoomStateModifierClassName())
+                {
+                    ___icon = class_name_sprite.Value;
+                    break;
+                }
+            }
+        }
+    }
+}

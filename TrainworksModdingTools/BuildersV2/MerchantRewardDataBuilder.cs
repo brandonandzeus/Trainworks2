14f55512066b6890075d2131ecf8ee390c3abd54
Brandon <tricksterguy87@gmail.com>
2024-03-20 14:20:51 -0700

    Add ability to create CustomMerchants.

---
+++ TrainworksModdingTools/BuildersV2/MerchantRewardDataBuilder.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+
+namespace Trainworks.BuildersV2
+{
+    public class MerchantRewardDataBuilder
+    {
+        public RewardData RewardData { get; set; }
+        public IRewardDataBuilder RewardDataBuilder { get; set; }
+        public MutatorData RequiredMutator { get; set; }
+        public bool OncePerVisit { get; set; }
+
+        public MerchantRewardDataBuilder()
+        {
+        }
+
+        public MerchantRewardData Build()
+        {
+            var merchantReward = new MerchantRewardData();
+
+            var rewardData = RewardData;
+            if (RewardDataBuilder != null)
+            {
+                rewardData = RewardDataBuilder.Build();
+            }
+
+            AccessTools.Field(typeof(MerchantRewardData), "rewardData").SetValue(merchantReward, rewardData);
+            AccessTools.Field(typeof(MerchantRewardData), "requiredMutator").SetValue(merchantReward, RequiredMutator);
+            AccessTools.Field(typeof(MerchantRewardData), "oncePerVisit").SetValue(merchantReward, OncePerVisit);
+
+            return merchantReward;
+        }
+    }
+}
--- TrainworksModdingTools/Managers/CustomMapNodePoolManager.cs
+++ TrainworksModdingTools/Managers/CustomMapNodePoolManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using BepInEx.Logging;
 using HarmonyLib;
+using Malee;
 using Trainworks.Builders;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -16,9 +18,10 @@ namespace Trainworks.Managers
         /// Reward nodes which naturally appear in the pool in the base game will not appear in these lists.
         /// </summary>
         public static IDictionary<string, List<RewardNodeData>> CustomRewardNodeData { get; } = new Dictionary<string, List<RewardNodeData>>();
+        public static IDictionary<string, List<MapNodeData>> CustomMapNodeData { get; } = new Dictionary<string, List<MapNodeData>>();
 
         /// <summary>
-        /// Add the card to the card pools with given IDs.
+        /// Add the map node to the map node pools with given IDs.
         /// </summary>
         /// <param name="rewardNodeData">RewardNodeData to be added to the pools</param>
         /// <param name="mapNodePoolIDs">List of map node pool IDs to add the reward node to</param>
@@ -33,6 +36,21 @@ namespace Trainworks.Managers
                     CustomRewardNodeData[mapNodePoolID] = new List<RewardNodeData>();
                 }
                 CustomRewardNodeData[mapNodePoolID].Add(rewardNodeData);
+            }
+        }
+
+        public static void RegisterCustomMerchant(MerchantData merchantData, List<string> mapNodePoolIDs)
+        {
+            var mapNodeDatas = (List<MapNodeData>)AccessTools.Field(typeof(AllGameData), "mapNodeDatas").GetValue(ProviderManager.SaveManager.GetAllGameData());
+            mapNodeDatas.Add(merchantData);
+            foreach (string mapNodePoolID in mapNodePoolIDs)
+            {
+                if (!CustomMapNodeData.ContainsKey(mapNodePoolID))
+                {
+                    CustomMapNodeData[mapNodePoolID] = new List<MapNodeData>();
+                }
+                CustomMapNodeData[mapNodePoolID].Add(merchantData);
+                AddCustomMapNodesToBucket(mapNodePoolID, merchantData);
             }
         }
 
@@ -70,6 +88,22 @@ namespace Trainworks.Managers
                 }
 
                 mapNodes.AddRange(validMapNodes);
+            }
+        }
+
+        public static void AddCustomMapNodesToBucket(string mapNodePoolID, MapNodeData data)
+        {
+            var runData = ProviderManager.SaveManager.GetAllGameData().GetBalanceData().GetRunData(false);
+            var mnbl = (ReorderableArray<MapNodeBucketList>)AccessTools.Field(typeof(RunData), "mapNodeBucketLists").GetValue(runData);
+
+            foreach (var bucketList in mnbl[0].BucketList)
+            {
+                var id = (string) AccessTools.Field(typeof(MapNodeBucketContainer), "id").GetValue(bucketList);
+                if (id == mapNodePoolID)
+                {
+                    var containerList = (ReorderableArray<MapNodeBucketData>) AccessTools.Field(typeof(MapNodeBucketContainer), "mapNodeBucketContainerList").GetValue(bucketList);
+                    containerList[0].MapNodes.Add(data);
+                }
             }
         }
     }

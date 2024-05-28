3336bdcd5e940e7947ea29cb7b1741738ae11377
Brandon <tricksterguy87@gmail.com>
2024-05-27 17:04:21 -0700

    Add IPostStartOfRunRelicEffect.cs
    RelicEffectInterface for an artifact that runs its effects after a run has been successfully setup.
    
    The use case for this is for ClassData#StarterRelics, If an artifact is given here it is added at the very very start of a run, before Covenant effects are ran. This is an issue since Covenant effects when ran will purge the initial starter deck and rebuild it from scratch. So if one wanted an artifact that works as Wurmtooth describes itself (Wurmtooth doesn't actually do anything) a Custom RelicEffect using IPostStartOfRunRelicEffect can be used to modify the starter deck without any additional patches.
    
    This is probably why Wurmtooth doesn't do anything, and why the enable_corruption flag exists in the ClassData class.

---
+++ TrainworksModdingTools/Custom/RelicEffects/IPostStartOfRunRelicEffect.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace Trainworks.CustomRelicEffects
+{
+    /// <summary>
+    /// Interface for a RelicEffect that runs its effects after a new run has been setup.
+    /// 
+    /// Very useful for the StarterRelics param for Custom clans that want to modify the starter deck.
+    /// A good example is a Wurmkin-like Relic that setups the deck and gives Starter Cards Infused.
+    /// 
+    /// If you use StarterRelics the artifacts specified will be given *AT THE VERY START* of a run
+    /// before the Convenants are setup. This is an issue since the Covenants effects will run and  will
+    /// remove all cards and then add the appropriate starter deck afterward thus negating any modifications 
+    /// you did to the starter deck.
+    /// </summary>
+    interface IPostStartOfRunRelicEffect : IRelicEffect
+    {
+        void ApplyEffect(RelicEffectParams param);
+    }
+}

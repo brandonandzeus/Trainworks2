4dce3136a1938c6e313aa68d51d4603e3ddf143a
Brandon <tricksterguy87@gmail.com>
2024-05-31 01:05:23 -0700

    Fix rare trigger localization key issue.
    The code apparently in one instance forms localization keys of the form Trigger_<triggerid>_CardText
    
    That works in the base game since the Trigger enum will evaluate to the enum name, but with custom triggers that won't work since it spits out the enum numerical value instead.

---
+++ TrainworksModdingTools/Patches/CustomTriggerNamePatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Trainworks.Enums.MTTriggers;
+
+namespace TrainworksModdingTools.Patches
+{
+    [HarmonyPatch(typeof(TooltipUI), nameof(TooltipUI.InitCardExplicitTooltip))]
+    public class CustomTriggerNamePatch
+    {
+        private static Regex pattern = new Regex(@"Trigger_(?<triggerid>\d+)_");
+
+        public static void Prefix(ref string titleKey, ref string descriptionKey)
+        {
+            if (titleKey == null || descriptionKey == null) return;
+
+            var match = pattern.Match(titleKey);
+            if (!match.Success) return;
+
+
+            var triggerId = (CharacterTriggerData.Trigger) Int32.Parse(match.Groups["triggerid"].Value);
+            var baseKey = CharacterTriggerData.TriggerToLocalizationExpression[triggerId];
+
+            titleKey = baseKey + "_CardText";
+            descriptionKey = baseKey + "_TooltipText";
+        }
+    }
+}

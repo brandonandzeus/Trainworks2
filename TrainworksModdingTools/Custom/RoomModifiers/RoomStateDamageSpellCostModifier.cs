2a0a874084ecdf206cee43ab5a16c769561ea2b8
Brandon <tricksterguy87@gmail.com>
2024-06-18 23:40:25 -0700

    Add correct versions of RoomStateDamageCostModifer and RoomStateHealCostModifer
    These originals have a bug that allows a monster card that has a spawn effect that grants regen the bonus cost reduction.
    And also checked if the cards have a permanent upgrade that damages or heals with an OnCast trigger
    
    The logic of these have been changed to target cards with a damage/healing effect (can be extended) OR cards with a trigger type of OnCast with a damage/healing effect (from a temp or permanent upgrade).

---
+++ TrainworksModdingTools/Custom/RoomModifiers/RoomStateDamageSpellCostModifier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.Custom.RoomModifiers
+{
+    /// <summary>
+    /// Simplified version of the Stock RoomStateDamageCostModifier with improvements.
+    /// The original has extraneous code that was checking for a condition (A monster with a spawn effect that applies regen) that doesn't exist.
+    /// This version checks for various Damaging card effects and can be extended across all mods, by mods with custom damage effects adding to 
+    /// the DamagingCardEffects list.
+    /// 
+    /// The original only checked for CardEffectDamage.
+    /// </summary>
+    public class RoomStateDamageSpellCostModifier : RoomStateCostModifierBase
+    {
+        public static List<Type> DamagingCardEffects = new List<Type>
+        {
+            typeof(CardEffectDamage),
+            typeof(CardEffectDamageByUnitsKilled),
+            typeof(CardEffectDamagePerTargetAttack),
+            typeof(CardEffectHealAndDamageRelative),
+        };
+
+        public override int GetModifiedCost(CardState cardState, CardStatistics cardStatistics, MonsterManager monsterManager)
+        {
+            if (cardState.GetCardType() != CardType.Spell)
+                return 0;
+
+            foreach (CardEffectState effectState in cardState.GetEffectStates())
+            {
+                if (DamagingCardEffects.Contains(effectState.GetCardEffect().GetType()))
+                {
+                    return modifiedCost;
+                }
+            }
+            foreach (CardTriggerEffectState effectTrigger in cardState.GetTriggers())
+            {
+                if (effectTrigger.GetTrigger() != CardTriggerType.OnCast)
+                {
+                    continue;
+                }
+                foreach (CardEffectState effectState in effectTrigger.GetCardEffectParams())
+                {
+                    if (DamagingCardEffects.Contains(effectState.GetCardEffect().GetType()))
+                    {
+                        return modifiedCost;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ TrainworksModdingTools/Custom/RoomModifiers/RoomStateHealSpellCostModifier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.Custom.RoomModifiers
+{
+    /// <summary>
+    /// Simplified version of the Stock RoomStateHealCostModifier with improvements.
+    /// The original has extraneous code that was checking for a condition (A monster with a spawn effect that applies regen) that doesn't exist.
+    /// This version checks for various Healing card effects and can be extended across all mods, by mods with custom heal effects that don't
+    /// inherit from CardEffectHeal adding to the OtherHealingCardEffects list.
+    /// 
+    /// The original only checked for CardEffectHeal (and subclasses).
+    /// </summary>
+    public class RoomStateHealSpellCostModifier : RoomStateCostModifierBase
+    {
+        public static List<Type> OtherHealingCardEffects = new List<Type>();
+
+        public override int GetModifiedCost(CardState cardState, CardStatistics cardStatistics, MonsterManager monsterManager)
+        {
+            if (cardState.GetCardType() != CardType.Spell)
+                return 0;
+
+            foreach (CardEffectState effectState in cardState.GetEffectStates())
+            {
+                if (typeof(CardEffectHeal).IsAssignableFrom(effectState.GetCardEffect().GetType()))
+                {
+                    return modifiedCost;
+                }
+                if (OtherHealingCardEffects.Contains(effectState.GetCardEffect().GetType()))
+                {
+                    return modifiedCost;
+                }
+            }
+            foreach (CardTriggerEffectState effectTrigger in cardState.GetTriggers())
+            {
+                if (effectTrigger.GetTrigger() != CardTriggerType.OnCast)
+                {
+                    continue;
+                }
+                foreach (CardEffectState effectState in effectTrigger.GetCardEffectParams())
+                {
+                    if (typeof(CardEffectHeal).IsAssignableFrom(effectState.GetCardEffect().GetType()))
+                    {
+                        return modifiedCost;
+                    }
+                    if (OtherHealingCardEffects.Contains(effectState.GetCardEffect().GetType()))
+                    {
+                        return modifiedCost;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}

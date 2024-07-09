2111998fa7cadea1eb4b9df32306d9285daf5fb2
Brandon <tricksterguy87@gmail.com>
2024-07-09 13:32:51 -0700

    Add a mechanism to report exceptions from Plugin.Initialize LOUDLY
    These exception messages will pop up at the main menu.

--- TrainworksModdingTools/Patches/InitializationPatches.cs
+++ TrainworksModdingTools/Patches/InitializationPatches.cs
@@ -1,10 +1,15 @@
-﻿using HarmonyLib;
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using I2.Loc;
 using ShinyShoe.Loading;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Trainworks.Interfaces;
 using Trainworks.Managers;
 using Trainworks.Utilities;
+using UnityEngine;
 
 namespace Trainworks.Patches
 {
@@ -28,11 +33,21 @@ namespace Trainworks.Patches
     [HarmonyPatch(typeof(AssetLoadingManager), "Start")]
     class AssetLoadingManagerInitializationPatch
     {
+        public static List<Tuple<Type, Exception>> FailedToLoadMods = new List<Tuple<Type, Exception>>();
+
         public static void Postfix(AssetLoadingData ____assetLoadingData)
         {
+            Trainworks.Log("[START] Loading all Trainworks Plugins");
+
             CustomCardPoolManager.GatherAllVanillaCardPools();
 
-            Trainworks.Log("[START] Loading all Trainworks Plugins");
+            var clm = CustomLocalizationManager.Instance();
+            if (!LocalizationManager.ParamManagers.Contains(clm))
+            {
+                LocalizationManager.ParamManagers.Add(clm);
+                LocalizationManager.LocalizeAll(true);
+            }
+
             // Ensure that unlockable cards are loaded, otherwise they will never be loaded.
             // (UnlockScreen doesn't have a call to LoadAdditionalCards).
             // All others are OK since in DraftRewardData the assets are loaded right then.
@@ -53,7 +68,21 @@ namespace Trainworks.Patches
                     .Where((plugin) => plugin is IInitializable)
                     .Select((plugin) => plugin as IInitializable)
                     .ToList();
-            initializables.ForEach((initializable) => initializable.Initialize());
+            initializables.ForEach((initializable) => DoInitialize(initializable));
+        }
+
+        public static void DoInitialize(IInitializable mod)
+        {
+            try
+            {
+                mod.Initialize();
+            }
+            catch (Exception ex)
+            {
+                var t = new Tuple<Type, Exception>(mod.GetType(), ex);
+                FailedToLoadMods.Add(t);
+                Trainworks.Log(LogLevel.Error, string.Format("[CATASTROPHIC] {0} Raised an exception: {1}", mod.GetType(), ex.Message));
+            }
         }
     }
 
@@ -61,7 +90,7 @@ namespace Trainworks.Patches
     class SpriteAssetInitializationPatch
     {
         private static bool HasBuiltSpriteAssets = false;
-        public static void Postfix()
+        public static void Postfix(ScreenManager ____screenManager)
         {
             // Here because not all clans Initialize via their Initialize (Equestrian)
             // Note for later, Not every custom GameData object is guaranteed to be loaded by clans.
@@ -72,8 +101,51 @@ namespace Trainworks.Patches
                 CustomLocalizationManager.ImportLocalizationData();
                 HasBuiltSpriteAssets = true;
                 Trainworks.Log("[END] All Trainworks Plugins are loaded.");
+
+                /*if (AssetLoadingManagerInitializationPatch.FailedToLoadMods.Count != 0)
+                {
+                    foreach (var type_ex in AssetLoadingManagerInitializationPatch.FailedToLoadMods)
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.AppendLine(string.Format("{0} failed to load due to the following Exception.", type_ex.Key.Name));
+                        stringBuilder.AppendLine(type_ex.Value.ToString());
+                        ____screenManager.ShowNotificationDialog(stringBuilder.ToString());
+                    }
+                }*/
             }
-            // Do not add any code here, as this function is called multiple times in diiffrent locations.
+            // Do not add any code here, as this function is called multiple times in different locations.
+        }
+    }
+
+    [HarmonyPatch(typeof(MainMenuScreen), "Initialize")]
+    class ErrorReportingPatch
+    {
+        static ScreenManager screenManager;
+        static int shown = 0;
+        static bool HasShownErrors = false;
+        public static void Postfix(ScreenManager ___screenManager)
+        {
+            screenManager = ___screenManager;
+            if (!HasShownErrors && AssetLoadingManagerInitializationPatch.FailedToLoadMods.Count != 0)
+            {
+                ShowException();
+            }
+            HasShownErrors = true;
+        }
+
+        public static void ShowException()
+        {
+            if (shown >= AssetLoadingManagerInitializationPatch.FailedToLoadMods.Count)
+                return;
+
+            (Type type, Exception ex) = AssetLoadingManagerInitializationPatch.FailedToLoadMods[shown];
+            shown++;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(string.Format("{0} failed to load due to the following Exception.", type.FullName));
+            stringBuilder.AppendLine(ex.ToString());
+            screenManager.ShowNotificationDialog(stringBuilder.ToString(), ShowException);
+            
         }
     }
 }
